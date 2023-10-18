0067c85f23109991a8c941f8450721646741a111
Nur Ridho A <abdurrahmansyah234@gmail.com>
2023-10-18 12:06:12 +0700

    - com.hutamakarya.bookseat - versionCode 2 - versionName "1.0.2"

--- android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -1,4 +1,4 @@
-package io.ionic.starter;
+package com.hutamakarya.bookseat;
 
 import com.getcapacitor.BridgeActivity;
 
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.hutamakarya.bookseat',
   appName: 'book-seat',
   webDir: 'www',
   server: {
