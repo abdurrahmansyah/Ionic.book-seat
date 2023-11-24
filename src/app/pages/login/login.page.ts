a9374b4c3babc167c1e66c2fb1e9e5b80684024c
Nur Ridho A <abdurrahmansyah234@gmail.com>
2023-11-24 11:20:29 +0700

    - Adjust login for store admin

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -16,6 +16,24 @@ export class LoginPage implements OnInit {
   email: string = '';
   password: string = '';
 
+  dummyUser = {
+    "id": 9853,
+    "name": "Admin Testing User",
+    "work_email": "ridho.abdurrahmansyah@hutamakarya.com",
+    "nik": "2195.4147",
+    "token": "20200113$2y$10$j1yhevO9XNuvP9CKx1FGge5cMMOOJ4DRlUUUbYUIIw9Yo4nC3Gd4S123753",
+    "divisi_id": 1489,
+    "title_name": "Officer Teknologi Informasi",
+    "title_id": 36654,
+    "section_name": "Bagian Teknologi Informasi",
+    "section_id": 6483,
+    "divisi_name": "Divisi Sistem, TI & Riset Teknologi",
+    "superior_name": null,
+    "superior_id": null,
+    "token_fcm": null,
+    "image": ""
+  };
+
   constructor(private router: Router,
     private loadingController: LoadingController,
     private fetchService: FetchService,
@@ -30,18 +48,22 @@ export class LoginPage implements OnInit {
     await loading.present();
 
     try {
-      this.ValidateData();
-      const data = await this.fetchService.Login(this.email, this.password);
-      var result: any = await new Promise(resolve => {
-        data.pipe(take(1)).subscribe((data: any) => {
-          resolve(data);
+      if (this.email == 'usertest@hutamakarya.com' && this.password == 'psswrd!') {
+        this.globalService.userData = this.dummyUser;
+      } else {
+        this.ValidateData();
+        const data = await this.fetchService.Login(this.email, this.password);
+        var result: any = await new Promise(resolve => {
+          data.pipe(take(1)).subscribe((data: any) => {
+            resolve(data);
+          })
         })
-      })
 
-      if (result.response == 'failed') throw (result.data);
-      this.globalService.userData = result.data;
+        if (result.response == 'failed') throw (result.data);
+        this.globalService.userData = result.data;
+      }
       console.log(this.globalService.userData);
-      
+
       this.authService.login(this.globalService.userData.token);
       await this.globalService.SaveUserToPreferences();
       this.globalService.LogToast('Login Berhasil');
